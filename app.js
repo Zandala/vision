b1c94c7feaf13b0e883c937a0362665e7f09c11d
adidag <adidag@qq.com>
2020-12-17 16:53:57 +0800

    test serverless

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 80
 app.get('/', (req, res) => res.send('Node-HK'))
 
 app.get('/api',(req, res) => {
-    axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=ARKK&outputsize=full&apikey=8PYGCMERXY9SSQ9K').then((response)=>{
+    axios.get('https://1155308185149296.cn-hangzhou.fc.aliyuncs.com/2016-08-15/proxy/app/api/').then((response)=>{
         res.send(response.data)
     }).catch(function (error) {
         res.send(error)
